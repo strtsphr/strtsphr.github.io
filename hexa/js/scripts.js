d26e3fdb729de2166fa2d648afb3c74d4938bb5c
rooandqoo <sa2ki.k32@gmail.com>
2016-05-30 02:13:49 +0900

    move sp site

--- hexa/js/scripts.js
+++ hexa/js/scripts.js
@@ -1,4 +1,12 @@
 $(function() {
+if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1)
+    || navigator.userAgent.indexOf('iPod') > 0
+    || navigator.userAgent.indexOf('Android') > 0) {
+      location.href = '../hexa-resp/';
+  }
+});
+
+$(function() {
   var h = $(window).height();
   $('.loader-bg ,.loader').height(h).css('display','block');
   $('.wrap').css('display','none');
